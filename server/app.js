9f1463cfcdfcd88d4f6230ffd0c737f94a44ece5
rohanN8740 <rohannandavdekar8983@gmail.com>
2026-09-07 03:45:24 +0530

    Prepare backend for deployment

--- server/app.js
+++ server/app.js
@@ -49,5 +49,5 @@ app.get("/health", (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
